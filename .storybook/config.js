fb8268416a1e44bef719d4e48d89f6d28f75e317
Ivan Stanishev <istanishev@cimpress.com>
2018-08-17 16:33:43 +0200

    * Add support for mutually exclusive roles * Replace toggles with checkboxes * Fix issues, add more storybook stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,15 @@
-import { configure } from '@storybook/react';
+import {configure} from '@storybook/react';
+import '@storybook/addon-console';
+
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({ addonPanelInRight: true });
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
+
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+    req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,8 +4,12 @@ import {storiesOf} from '@storybook/react';
 import Authenticated from './Authenticated';
 import {Drawer} from '@cimpress/react-components';
 import UsersTable from '../src/components/UsersTable';
+import {withKnobs, object, boolean, number} from '@storybook/addon-knobs/react';
 
-import auth from './auth'
+import auth from './auth';
+
+const stories = storiesOf('UsersTable', module);
+stories.addDecorator(withKnobs);
 
 let roles = [{
     roleName: 'Template Editor',
@@ -17,34 +21,44 @@ let roles = [{
     isManagerRole: false,
 }];
 
-storiesOf('UsersTable', module)
-    .add('Basic use', () => <Authenticated><UsersTable
-        groupId={5363}
-        allowedRoles={roles}
-        showAdminsOnly={false}
-    /></Authenticated>)
+stories.add('Basic use', () => <Authenticated><UsersTable
+    groupId={number('Group ID', 5223)}
+    allowedRoles={object('Allowed Roles', roles)}
+    showAdminsOnly={boolean('Admins only', false)}
+    showGroupInfo={boolean('Group Info', false)}
+    mutuallyExclusiveRoles={boolean('Mutually Exclusive Roles', false)}
+/></Authenticated>);
 
-    .add('Show admins only', () => <Authenticated><UsersTable
-        groupId={5363}
-        allowedRoles={roles}
-        showAdminsOnly={true}
-    /></Authenticated>)
+stories.add('Mutually exclusive roles', () => <Authenticated><UsersTable
+    groupId={number('Group ID', 5223)}
+    allowedRoles={object('Allowed Roles', roles)}
+    showAdminsOnly={boolean('Admins only', false)}
+    showGroupInfo={boolean('Group Info', false)}
+    mutuallyExclusiveRoles={boolean('Mutually Exclusive Roles', true)}
+/></Authenticated>);
 
-    .add('With group info', () => <Authenticated><UsersTable
-        groupId={5363}
-        allowedRoles={roles}
-        showAdminsOnly={false}
-        showGroupInfo
-    /></Authenticated>)
+stories.add('Show admins only', () => <Authenticated><UsersTable
+    groupId={number('Group ID', 5223)}
+    allowedRoles={object('Allowed Roles', roles)}
+    showAdminsOnly={boolean('Admins only', true)}
+    showGroupInfo={boolean('Group Info', false)}
+/></Authenticated>);
 
-    .add('In a drawer', () => <Authenticated>
-        <Drawer
-            show={true}
-            header="The drawer header"
-            footer={<div>The drawer footer</div>}>
-            <UsersTable
-                accessToken={auth.getAccessToken()}
-                groupId={5363}
-                allowedRoles={roles}
-            />
-        </Drawer></Authenticated>);
+stories.add('With group info', () => <Authenticated><UsersTable
+    groupId={number('Group ID', 5223)}
+    allowedRoles={object('Allowed Roles', roles)}
+    showAdminsOnly={boolean('Admins only', false)}
+    showGroupInfo={boolean('Group Info', true)}
+/></Authenticated>);
+
+stories.add('In a drawer', () => <Authenticated>
+    <Drawer
+        show={true}
+        header="The drawer header"
+        footer={<div>The drawer footer</div>}>
+        <UsersTable
+            accessToken={auth.getAccessToken()}
+            groupId={number('Group ID', 5223)}
+            allowedRoles={object('Allowed Roles', roles)}
+        />
+    </Drawer></Authenticated>);
